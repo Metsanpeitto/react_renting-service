6d9c50c97ef7d700ff7776b16273409bec929455
Metsanpeitto <abraham@waldenberginc.com>
2021-12-13 17:55:18 +0100

    Add addItem and DeleteItem to the items handler

--- src/api/items.js
+++ src/api/items.js
@@ -11,6 +11,44 @@ const getItems = async () => axios.get(`${url}`).then((result) => {
     return items;
 });
 
+const deleteItem = async (itemId) => {
+    const token = localStorage.getItem("token");
+    const config = {
+        headers: {
+            Authorization: token
+        }
+    }
+    axios.delete(`${url}/${itemId}`, config).then((result) => {
+        let items = [];
+        if (result.status === 200) {
+            items = result.data;
+        }
+        return result;
+    });
+}
+
+
+
+const addItem = async (item) => {
+    const token = localStorage.getItem("token");
+    const config = {
+        headers: {
+            Authorization: token
+        }
+    }
+    const body = {
+        user_id: item.userId,
+        name: item.name,
+        description: item.description,
+        image: item.image,
+        price: item.price,
+        color: item.color,
+    };
+
+    const response = axios.post(`${url}`, body, config)
+        .then((result) => result.data);
+    return response;
+}
 
 
 
