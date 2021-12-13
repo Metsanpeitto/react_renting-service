ea4f5af5c6cc099b62b7381623617fafc29cb36a
Metsanpeitto <abraham@waldenberginc.com>
2021-12-13 17:13:09 +0100

    Add configureStore where reducers are combined

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,5 +14,5 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
--- src/redux/items/items.js
+++ src/redux/items/items.js
@@ -3,24 +3,24 @@ import { ADD_ITEM, RECEIVE_ITEMS } from '../constants/action-types';
 const initialState = { items: [] };
 
 export const addItem = (payload) => ({
-    type: ADD_ITEM,
-    payload,
+  type: ADD_ITEM,
+  payload,
 });
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_ITEM: {
-            const items = [...state.items, action.payload];
-            return { ...state, items };
-        }
-        case RECEIVE_ITEMS: {
-            const { items } = action;
-            return { items };
-        }
-
-        default:
-            return state;
+  switch (action.type) {
+    case ADD_ITEM: {
+      const items = [...state.items, action.payload];
+      return { ...state, items };
     }
+    case RECEIVE_ITEMS: {
+      const { items } = action;
+      return { items };
+    }
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
