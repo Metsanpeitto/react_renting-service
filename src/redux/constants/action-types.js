2c3194a8063538a53788a909e0f7ea23c4f3db57
Metsanpeitto <abraham@waldenberginc.com>
2021-12-13 18:40:36 +0100

    Add user crelated constants to the constants-types file

--- src/redux/constants/action-types.js
+++ src/redux/constants/action-types.js
@@ -3,3 +3,8 @@ export const RECEIVE_RESERVATIONS = 'RECEIVE_RESERVATIONS';
 export const ADD_ITEM = 'ADD_ITEM';
 export const RECEIVE_ITEMS = 'RECEIVE_ITEMS';
 export const DELETE_ITEM = 'DELETE_ITEM';
+export const SIGN_UP = 'SIGN_UP';
+export const SIN_IN = 'SIN_IN';
+export const RECEIVE_USER = 'RECEIVE_USER';
+export const RESET_USER = 'RESET_USER';
+
