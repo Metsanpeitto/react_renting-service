fcb5c8e51ff7670e3721fa7a0697d06f8499383e
Akasha <Akasha.Rojee@gmail.com>
2021-12-13 21:09:24 +0400

    Fix linter errors

--- src/features/newItem/NewItem.js
+++ src/features/newItem/NewItem.js
@@ -1,32 +1,30 @@
-/* eslint-disable */
 import styles from './NewItem.module.scss';
 
 function NewItem() {
-
-    return (
-        <div className={styles.container}>
-            <h2>ADD NEW ITEM</h2>
-            <form>
-                <input
-                    placeholder="Name"
-                    type="text"
-                />
-                <input
-                    placeholder="Description"
-                    type="text"
-                />
-                <input
-                    placeholder="Image"
-                    type="text"
-                />
-                <input
-                    placeholder="Price"
-                    type="number"
-                />
-                <input type="submit" value="Add Item" />
-            </form>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h2>ADD NEW ITEM</h2>
+      <form>
+        <input
+          placeholder="Name"
+          type="text"
+        />
+        <input
+          placeholder="Description"
+          type="text"
+        />
+        <input
+          placeholder="Image"
+          type="text"
+        />
+        <input
+          placeholder="Price"
+          type="number"
+        />
+        <input type="submit" value="Add Item" />
+      </form>
+    </div>
+  );
 }
 
 export default NewItem;
