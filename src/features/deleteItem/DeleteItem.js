afc8dee321cc155106a28547d9160c4b22454c41
Akasha <Akasha.Rojee@gmail.com>
2021-12-15 03:13:11 +0400

    Add functionality to delete page

--- src/features/deleteItem/DeleteItem.js
+++ src/features/deleteItem/DeleteItem.js
@@ -1,19 +1,51 @@
 /* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getItems, deleteItem } from '../redux/api/api';
+import { useNavigate } from "react-router-dom";
+import user from '../api/user';
 
 const DeleteItem = () => {
+    const user = useSelector((state) => state.userReducer.user);
+    const items = useSelector((state) => state.itemsReducer.items);
+    const dispatch = useDispatch();
+    const [calledItems, setCalledItems] = useState(null);
+    let navigate = useNavigate();
 
-  return (
-    <section>
-      { calledItems.map((item) => {
-        return (
-          <div>
-            <h4>{item.name}</h4>
-            <button id={item.id}>Delete Item</button>
-          </div>
-        )
-    }) }
-    </section>
-  );
+    useEffect(() => {
+        console.log("Hi");
+        if (!calledItems && items.length === 0) {
+            dispatch(getItems());
+            console.log(items);
+        }
+        if (!calledItems || calledItems !== items) {
+            setCalledItems(items);
+        }
+    });
+
+    const submitDeleteToStore = (e) => {
+        const itemId = e.currentTarget.id;
+        dispatch(deleteItem(itemId));
+        dispatch(getItems());
+    };
+
+    return (
+        <section>
+            {user.id ? <div>
+                {calledItems ?
+                    calledItems.map(item => {
+                        return (
+                            <div>
+                                <h4>{item.name}</h4>
+                                -
+                                <button id={item.id} onClick={(e) => submitDeleteToStore(e)}>Delete Item</button>
+                            </div>
+                        )
+                    }) : null
+                }
+            </div> : null}
+        </section>
+    )
 };
 
 export default DeleteItem;
