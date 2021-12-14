f6f47eaff2a6cc41b3f7d10e11df99f9eecc6a2b
Akasha <Akasha.Rojee@gmail.com>
2021-12-14 15:01:23 +0400

    Get items for main page

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,12 +1,33 @@
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getItems } from '../../redux/api/api';
+
 import Carousel from './carousel/Carousel';
 import styles from './Main.module.scss';
 
-const Main = () => (
-  <div className={styles.main}>
-    <h1>LATEST MODELS</h1>
-    <p>Please select a model</p>
-    <Carousel />
-  </div>
-);
+const Main = () => {
+
+  const items = useSelector((state) => state.itemsReducer.items);
+  const dispatch = useDispatch();
+  const [calledItems, setCalledItems] = useState(null);
+
+  useEffect(() => {
+    console.log("...getting items...");
+    if (!calledItems && items.length === 0) {
+      setCalledItems(true);
+      dispatch(getItems());
+    }
+    console.log(items);
+  });
+
+  return (
+    <div className={styles.main}>
+      <h1>LATEST MODELS</h1>
+      <p>Please select a model</p>
+      <Carousel />
+    </div>
+  );
+};
 
 export default Main;
