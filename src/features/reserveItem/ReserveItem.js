28a7cb2df9053b507008de61eda5672b45cbce07
Akasha <Akasha.Rojee@gmail.com>
2021-12-14 12:23:39 +0400

    Style reserve form

--- src/features/reserveItem/ReserveItem.js
+++ src/features/reserveItem/ReserveItem.js
@@ -2,24 +2,26 @@
 import React from "react";
 import DateTimePicker from 'react-datetime-picker';
 import Select from 'react-select';
+import styles from './ReserveItem.module.scss';
 
 function ReserveItem() {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>RESERVE ITEM</h2>
-      
+
       <form>
 
         <input
           placeholder="City"
           type="text"
+          className={styles.input}
         />
 
-        <DateTimePicker />
+        <DateTimePicker className={`${styles.input} ${styles.data}`} />
 
-        <Select />
+        <Select className={styles.input} />
 
-        <input type="submit" value="Reserve Item" />
+        <input className={styles.input} type="submit" value="Reserve Item" />
 
       </form>
     </div>
