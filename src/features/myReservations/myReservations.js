7bc0570403bffca1812db994ce6231e56b0aa30a
Akasha <Akasha.Rojee@gmail.com>
2021-12-14 23:43:05 +0400

    Add functionality to reservations page

--- src/features/myReservations/myReservations.js
+++ src/features/myReservations/myReservations.js
@@ -1,6 +1,22 @@
 /* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getReservations } from '../../redux/api/api';
 
 const MyReservations = () => {
+
+  const reservations = useSelector((state) => state.reservationsReducer.reservations);
+  const dispatch = useDispatch();
+  const [calledReservations, setCalledReservations] = useState(null);
+
+  useEffect(() => {
+      console.log("Hi");
+      if (!calledReservations && reservations.length === 0) {
+          setCalledReservations(true);
+          dispatch(getReservations());
+      }
+      console.log(reservations);
+  });
 
   return (
     <section>
