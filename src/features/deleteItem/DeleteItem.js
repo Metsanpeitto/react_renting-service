26adf2a4a69564b662efd8cd96e360fac2452c84
Akasha <Akasha.Rojee@gmail.com>
2021-12-15 18:49:57 +0400

    Add functionality to delete form

--- src/features/deleteItem/DeleteItem.js
+++ src/features/deleteItem/DeleteItem.js
@@ -1,6 +1,32 @@
 /* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getItems, deleteItem } from '../redux/api/api';
+import { useNavigate } from "react-router-dom";
 
 const DeleteItem = () => {
+    const user = useSelector((state) => state.userReducer.user);
+    const items = useSelector((state) => state.itemsReducer.items);
+    const dispatch = useDispatch();
+    const [calledItems, setCalledItems] = useState(null);
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("Hi");
+        if (!calledItems && items.length === 0) {
+            dispatch(getItems());
+            console.log(items);
+        }
+        if (!calledItems || calledItems !== items) {
+            setCalledItems(items);
+        }
+    });
+
+    const submitDeleteToStore = (e) => {
+        const itemId = e.currentTarget.id;
+        dispatch(deleteItem(itemId));
+        dispatch(getItems());
+    };
 
     return (
         <>
@@ -9,15 +35,14 @@ const DeleteItem = () => {
                     calledItems.map(item => {
                         return (
                             <div>
-                                <h4>{item.name}</h4>
+                                <h4 >{item.name}</h4>
                                 -
-                                <button id={item.id}>Delete Item</button>
+                                <button id={item.id} onClick={(e) => submitDeleteToStore(e)}>Delete Item</button>
                             </div>
                         )
                     }) : null
                 }
             </div> : null}
-
         </>
     )
 };
