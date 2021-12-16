c61a19d4ab5722c2275611798f11c27fa8b2c173
Metsanpeitto <abraham@waldenberginc.com>
2021-12-16 14:03:53 +0100

    Add item name to render on my reservations page

--- src/features/myReservations/MyReservations.js
+++ src/features/myReservations/MyReservations.js
@@ -5,12 +5,14 @@ import { getReservations } from '../../redux/api/api';
 import styles from './MyReservations.module.scss';
 
 const MyReservations = () => {
-
+  const items = useSelector((state) => state.itemsReducer.items);
   const reservations = useSelector((state) => state.reservationsReducer.reservations);
   const dispatch = useDispatch();
   const [calledReservations, setCalledReservations] = useState(null);
 
   useEffect(() => {
+    console.log(items)
+    console.log(reservations)
     if (!calledReservations && reservations.length === 0 && calledReservations !== reservations) {
       setCalledReservations(true);
       dispatch(getReservations());
@@ -21,16 +23,19 @@ const MyReservations = () => {
     <div className={styles.container}>
       <h2>MY RESERVATIONS</h2>
       <div className={styles.cards}>
-      {
-        reservations.map(reserve => {
-          return (
-            <div className={styles.card}>
-              <h4>{reserve.city}</h4>
-              <h4>{reserve.date}</h4>
-            </div>
-          )
-        })
-      }
+        {
+          reservations.map(reserve => {
+            let item = items.find(item => item.id === reserve.item_id)
+            console.log(item.name);
+            return (
+              <div className={styles.card}>
+                <h4>{item.name}</h4>
+                <h4>{reserve.city}</h4>
+                <h4>{reserve.date}</h4>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
