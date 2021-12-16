bfc68fb7d66b49a0defd7859aab7c203678d9e9b
Metsanpeitto <abraham@waldenberginc.com>
2021-12-16 17:46:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const cors = require("cors")
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.use(express.static(path.join(__dirname, 'build')));
 
