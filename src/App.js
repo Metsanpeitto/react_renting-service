6730552b52bc5b3d4075c1ef91b777a17dc73e2f
Metsanpeitto <abraham@waldenberginc.com>
2021-12-16 22:07:30 +0100

    Fix merge

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import NewItem from './features/newItem/NewItem';
 import NewReservation from './features/newReservation/NewReservation';
 import User from './features/user/User';
 import SignUp from './features/user/SignUp';
-import Details from './features/details/Details';
 import MyReservations from './features/myReservations/MyReservations';
+import Details from './features/details/Details';
 import Delete from './features/deleteItem/DeleteItem';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -60,7 +60,9 @@ function App() {
             <Route exact path="/" element={<Main />} />
             <Route path="/details/:itemName" element={<Details />} />
             <Route path="/add" element={<NewItem />} />
+
             <Route path="/new_reservation/:itemId" element={<NewReservation />} />
+
             <Route path="/my_reservations" element={<MyReservations />} />
             <Route path="/user" element={<User />} />
             <Route path="/sign_up" element={<SignUp />} />
